0d73b15ee718a9d4a3282058154ed022387d65f4
saquibb <saquibmsurs@gmail.com>
2020-06-20 20:53:15 +0500

    numeric age diffrentiating

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
 
-function App({ name = 'User' }) {
+function App({ name = 'User', age }) {
   return (
     <div>
-      Hello <strong>{name}</strong>!
+      Hello <strong>{name}</strong>! Age={age - 10}
       <br />
       <Hello />
     </div>
