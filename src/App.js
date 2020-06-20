0930c6dd677ff4118f72196bb9b2f71e868e8083
saquibb <saquibmsurs@gmail.com>
2020-06-20 20:33:55 +0500

    another element added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
-function App({ name }) {
+function App({ name = 'User' }) {
   return (
     <div>
       Hello <strong>{name}</strong>!
       <br />
+      <Hello />
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hello({}) {
+  return (
+    <div>
+      <h2>Drinks </h2>
+      <ol>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>drink</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Hello;
